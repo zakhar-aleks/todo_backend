3eb08e650367b474a60e285b6f5c8655b0cd8955
Захар Александрук <zaharaleksandruk@MacBook-Air-Zahar.local>
2025-11-16 20:15:16 +0200

    Add password hashing

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,33 @@ import express from "express";
 import { PrismaClient } from "./generated/prisma";
 import { validate } from "./middleware/validate.js";
 import * as z from "zod";
+import * as argon2 from "@node-rs/argon2";
 
 const prisma = new PrismaClient();
 const app = express();
 const port: string | number = process.env.port || 3000;
-
 app.use(express.json());
+
+interface PrismaClientError {
+	code: string;
+	message: string;
+}
+
+const isPrismaError = (error: unknown): error is PrismaClientError => {
+	return typeof error === "object" && error !== null && "code" in error;
+};
+
+async function hashPassword(plainPassword: string): Promise<string> {
+	try {
+		const hashedPassword: string = await argon2.hash(plainPassword);
+
+		return hashedPassword;
+	} catch (error) {
+		console.error("Error hashing password:", error);
+
+		throw new Error("Password hashing failed");
+	}
+}
 
 const User = z.object({
 	email: z.email().regex(/^[^\s@]+@[^\s@]+\.[^\s@]+$/),
@@ -25,7 +46,7 @@ app.post("/api/auth/registration/", validate(User), async (req, res) => {
 	const data: any = {
 		email: email,
 		name: name,
-		password: password,
+		password: await hashPassword(password),
 	};
 
 	if (avatar) {
@@ -39,8 +60,20 @@ app.post("/api/auth/registration/", validate(User), async (req, res) => {
 
 		res.status(201).json(newUser);
 	} catch (error) {
-		res.status(409).json({
-			error: error,
+		if (isPrismaError(error) && error.code === "P2002") {
+			return res.status(409).json({
+				error: "A user with this email is already registered",
+			});
+		}
+
+		console.error("Uncaught DB Error:", error);
+
+		const message = isPrismaError(error)
+			? error.message
+			: "An unexpected server error occurred.";
+		return res.status(500).json({
+			error: "An unexpected server error occurred.",
+			details: message,
 		});
 	}
 });
