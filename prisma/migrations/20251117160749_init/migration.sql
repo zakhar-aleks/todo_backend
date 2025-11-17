6b5d4f82a472bb78ee9b7af0cc5574b9dfbcb424
Захар Александрук <zaharaleksandruk@MacBook-Air-Zahar.local>
2025-11-17 18:11:24 +0200

    Create docker containers and migrate the project to postgresql

--- prisma/migrations/20251117160749_init/migration.sql
+++ prisma/migrations/20251117160749_init/migration.sql
@@ -1,9 +1,12 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "avatar" TEXT
+    "password" TEXT NOT NULL,
+    "avatar" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
